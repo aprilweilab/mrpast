7cb65a3cd714c4fd963b5697998fe657d6be8abb
Drew DeHaas <drewdehaas@gmail.com>
2026-07-01 15:32:08 -0400

    Update requirements.txt for doc build; switch themes (#57)
    Missing requirements for RTD were causing the API docs to not
    be generated properly.

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,6 @@ import sys
 sys.path.insert(0, os.path.abspath('../'))
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary"]
-html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 autosummary_generate = True
 project = "mrpast"
